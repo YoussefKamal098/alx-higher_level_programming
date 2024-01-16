26728513020c738668861f742b1e961a5c507622
YoussefKamal098 <kmalywsf098@gmail.com>
2024-01-16 22:03:16 +0200

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print(f"{i:02d}", end="")
+    print("{:02d}".format(i), end="")
     print(", " if i != 99 else "", end="" if i != 99 else "\n")
 
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end="")
-        print(f", " if i != 99 else "", end="" if i != 99 else "\n")
+        print("{}{}".format(i, j), end="")
+        print(", " if i != 99 else "", end="" if i != 99 else "\n")
