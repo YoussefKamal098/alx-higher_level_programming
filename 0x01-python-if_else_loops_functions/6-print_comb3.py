135f41dd49f389129f4bed06ef0a6ff6bee7455f
YoussefKamal098 <kmalywsf098@gmail.com>
2024-01-16 22:01:14 +0200

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         print(f"{i}{j}", end="")
-        print(", " if i != 99 else "", end="" if i != 99 else "\n")
+        print(f", " if i != 99 else "", end="" if i != 99 else "\n")
