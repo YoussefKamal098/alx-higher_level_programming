e1ab2859f3666ea30a285f1e13afd1c7fd49d5fd
YoussefKamal098 <kmalywsf098@gmail.com>
2024-01-16 22:13:11 +0200

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         print("{}{}".format(i, j), end="")
-        print(", " if i != 9 else "", end="" if i != 9 else "\n")
+        print(", " if i != 8 else "", end="" if i != 8 else "\n")
