a0dea6fdfd00b366347ef5fc085376372e7668b6
YoussefKamal098 <kmalywsf098@gmail.com>
2024-02-07 11:32:08 +0200

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Parameters:
+            value (int): The new size of the square.
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+
+    def my_print(self):
+        """ Prints a representation of the square using the '#' """
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="\n" if j is self.size - 1 and i != j else "")
+        print()
