6227d6ec168273f52b8a507c8fd86895c646440e
YoussefKamal098 <kmalywsf098@gmail.com>
2024-02-07 12:09:19 +0200

    update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -85,18 +85,15 @@ class Square:
     def my_print(self):
         """ Prints a representation of the square using the '#' with position """
         if self.__size == 0:
-            print("")
+            print()
             return
 
         for i in range(0, self.__position[1]):
-            print("")
+            print()
 
         for i in range(0, self.__size):
             for j in range(0, self.__position[0]):
                 print(" ", end="")
             for k in range(0, self.__size):
                 print("#", end="")
-
-            print("")
-
-        print()
+            print()
