9cfee8cad05f8922871f7228ea1ab93a514f0cc0
YoussefKamal098 <kmalywsf098@gmail.com>
2024-02-12 23:57:16 +0200

    update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -22,95 +22,93 @@ class SolveNQueens:
     SolveNQueens Class
 
     A class that provides a solution to the N-Queens problem.
-    The N-Queens problem involves placing N queens on an N x N
-    chessboard in such a way that no two queens threaten each other.
+    The N-Queens problem involves placing N queens on an N x N chessboard
+    in such a way that no two queens threaten each other.
 
     Attributes:
-        __n (int): The size of the chessboard and the number of queens.
-        __solution (List[List[int]]): A list to store possible solution,
-        where each solution is represented by a list of queen positions.
+        __n (int): The default size of the chessboard and the number of queens.
+        __solution (List): A list to store the current solution.
+        __solutions (List[List[List[int]]]): A list to store all solutions.
         __cols (set): A set to track occupied columns.
         __pos_diagonal (set): A set to track occupied positive diagonals.
         __neg_diagonal (set): A set to track occupied negative diagonals.
-        __solutions (List[List[List[int]]]):A list to store all possible
-        solutions.
 
     Methods:
-        __init__(self, n): Initializes the SolveNQueens object.
-        __solve(self): Solves the N-Queens problem using backtracking.
-        __backtrack(self, row): Recursive backtracking function to find
-        solutions.
+        solve(cls, n): Solves the N-Queens problem for a given
+        size of the chessboard.
+        __backtrack(cls, row, n): Recursive backtracking function
+        to find solutions.
     """
+    __n = 4
+    __solution = []
+    __solutions = []
+    __cols = set()
+    __pos_diagonal = set()
+    __neg_diagonal = set()
 
-    def __init__(self, n=4):
+    @classmethod
+    def solve(cls, n=4):
         """
-        Initialize the SolveNQueens object.
+        Solve the N-Queens problem for a given size of the chessboard.
 
         Args:
             n (int): The size of the chessboard and the number of queens.
-        """
 
+        Returns:
+            List[List[List[int]]]: A list of solutions, where each solution
+            is represented by a list of queen positions.
+        """
         if type(n) is not int:
-            raise TypeError("N must be integer")
+            raise TypeError("N must be a integer")
         if n < 4:
             raise ValueError("N must be at least 4")
 
-        self.__n = n
-        self.__solution = []
-        self.__solutions = []
-        self.__cols = set()
-        self.__pos_diagonal = set()
-        self.__neg_diagonal = set()
+        cls.__backtrack(0, n)
 
-    def solve(self):
-        """
-        Solve the N-Queens problem using backtracking.
+        return cls.__solutions
 
-        Returns:
-            List[List[List[int]]]: A list of solutions,
-            where each solution is represented
-            by a list of queen positions.
-        """
-        self.__backtrack(0)
-
-        return self.__solutions
-
-    def __backtrack(self, row):
+    @classmethod
+    def __backtrack(cls, row, n):
         """
         Recursive backtracking function to find solutions.
 
         Args:
             row (int): The current row being explored.
+            n (int): The size of the chessboard and the number of queens.
         """
-
         if type(row) is not int:
             raise TypeError("row must be integer")
         if row < 0:
             raise ValueError("row must be positive")
 
-        if row == self.__n:
-            self.__solutions.append([
-                [row, col] for row, col in self.__solution
+        if type(n) is not int:
+            raise TypeError("N must be a integer")
+        if n < 4:
+            raise ValueError("N must be at least 4")
+
+        if row == n:
+            cls.__solutions.append([
+                [row, col] for row, col in cls.__solution
             ])
             return
 
-        for col in range(self.__n):
-            if (col in self.__cols or
-                    (row + col) in self.__pos_diagonal or
-                    (row - col) in self.__neg_diagonal):
+        for col in range(n):
+            if (col in cls.__cols or
+                    (row + col) in cls.__pos_diagonal or
+                    (row - col) in cls.__neg_diagonal):
                 continue
 
-            self.__solution.append([row, col])
-            self.__pos_diagonal.add(row + col)
-            self.__neg_diagonal.add(row - col)
-            self.__cols.add(col)
+            cls.__solution.append([row, col])
+            cls.__pos_diagonal.add(row + col)
+            cls.__neg_diagonal.add(row - col)
+            cls.__cols.add(col)
 
-            self.__backtrack(row + 1)
+            cls.__backtrack(row + 1, n)
 
-            self.__solution.pop()
-            self.__pos_diagonal.discard(row + col)
-            self.__neg_diagonal.discard(row - col)
-            self.__cols.discard(col)
+            cls.__solution.pop()
+            cls.__pos_diagonal.discard(row + col)
+            cls.__neg_diagonal.discard(row - col)
+            cls.__cols.discard(col)
 
 
 if __name__ == "__main__":
@@ -125,8 +123,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     n = int(sys.argv[1])
-    solution = SolveNQueens(n)
+    solutions = SolveNQueens.solve(n)
 
-    solutions = solution.solve()
     for solution in solutions:
         print(solution)
