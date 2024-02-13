582aba63ab455ecee9d7d12933886083abe26d8a
YoussefKamal098 <kmalywsf098@gmail.com>
2024-02-13 20:52:19 +0200

    102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+
+/**
+ * print_python_string - prints a basic information about a Python string
+ * @p: pointer to a Python object
+ */
+void print_python_string(PyObject *p)
+{
+	wprintf(L"[.] string object info\n");
+
+	if (strcmp(p->ob_type->tp_name, "str"))
+	{
+		wprintf(L"  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		wprintf(L"  type: compact ascii\n");
+	else
+		wprintf(L"  type: compact unicode object\n");
+
+	wprintf(L"  length: %lu\n", PyUnicode_GET_LENGTH(p));
+	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
+}
