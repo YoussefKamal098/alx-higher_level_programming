d60b5ac0d8d30d333fa7a70fb088864d8639b916
YoussefKamal098 <kmalywsf098@gmail.com>
2024-02-19 19:07:03 +0200

    update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,11 +4,13 @@
 
 def is_kind_of_class(obj, a_class):
     """
-    Check if the object is an instance of the specified class or any of the classes in a tuple.
+    Check if the object is an instance of the specified class
+    or any of the classes in a tuple.
 
     Parameters:
     - obj: Any, the object to be checked.
-    - a_class: Type or Tuple[Type], the class or tuple of classes for comparison.
+    - a_class: Type or Tuple[Type], the class or tuple
+    of classes for comparison.
 
     Returns:
     - bool: True if obj is an instance of a_class, False otherwise.
