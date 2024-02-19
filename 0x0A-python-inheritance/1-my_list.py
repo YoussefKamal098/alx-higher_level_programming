b9cba9acad8bdec6b9aded5aa8b2f0480e175e79
YoussefKamal098 <kmalywsf098@gmail.com>
2024-02-19 18:38:52 +0200

    1-my_list.py - tests/1-my_list.txt

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module for MyList class."""
+
+
+class MyList(list):
+    """
+    A custom list class derived from the built-in list class.
+
+    This class inherits all the functionalities of the list class and
+    adds a method to print the sorted version of the list.
+
+    Attributes:
+        No additional attributes.
+
+    Methods:
+        print_sorted(): Prints the elements of the list in sorted order.
+    """
+
+    def print_sorted(self):
+        """
+        Print the elements of the list in sorted order.
+        """
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testfile("./tests/1-my_list.txt")
