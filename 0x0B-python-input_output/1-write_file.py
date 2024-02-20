a6218112ecacb5d5ab1397d7e2fcddb0aefdec2c
YoussefKamal098 <kmalywsf098@gmail.com>
2024-02-20 12:04:17 +0200

    update 0-read_file.py 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module provides utility functions for reading files in Python.
+
+Functions:
+- write_file(filename="", text=""): Write the specified text to a file.
+
+Parameters:
+- filename (str): The name of the file to be written. Default is an empty string.
+- text (str): The text to be written to the file. Default is an empty string.
+
+Raises:
+- FileNotFoundError: If the specified file is not found (for read_file).
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Write the specified text to a file.
+
+    Parameters:
+    - filename (str): The name of the file to be written. Default is an empty string.
+    - text (str): The text to be written to the file. Default is an empty string.
+
+    Returns:
+    - int: The number of characters written to the file.
+    """
+    with open(filename, "w") as file:
+        return file.write(text)
