a2aea3288da49f62339a1832cc056f0d2845609b
YoussefKamal098 <kmalywsf098@gmail.com>
2024-04-22 22:37:53 +0200

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-array = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const line of array) {
   console.log(line);
