5c638374fe9d8ed2a84fe4a571740f898b52eab1
YoussefKamal098 <kmalywsf098@gmail.com>
2024-04-24 17:42:24 +0200

    semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 3) {
-	console.log('0');
+  console.log('0');
 } else {
-	const array = process.argv.slice(2).map(Number);
-	const second = array.sort(function (a, b) { return b - a; })[1];
-	console.log(second);
+  const array = process.argv.slice(2).map(Number);
+  const second = array.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
