4aa21ce6186a1d67578de65a6bb141f3a7578a84
YoussefKamal098 <kmalywsf098@gmail.com>
2024-07-10 22:24:51 +0300

    update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -22,5 +22,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    # print(requests.post(url, data={"email": email}).text)
-    print(requests.post(url, json={"email": email}).text)
+    print(requests.post(url, data={"email": email}).text)
